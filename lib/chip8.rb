0390d5c4e5143ad2fd3c5efc97621107889ab656
João Seixas <up201505648@fe.up.pt>
2021-10-02 11:50:41 +0100

    setup project

---
+++ lib/chip8.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chip8/version"
+
+module Chip8
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/chip8_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Chip8 do
+  it "has a version number" do
+    expect(Chip8::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
